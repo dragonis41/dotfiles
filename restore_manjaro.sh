f2db70dd811d96e9791b3ad7c639c78f6136d159
Guillaume Pinon <dev@guillaume-pinon.eu>
2023-05-22 11:37:50 +0200

    init: add save and restore scripts

---
+++ restore_manjaro.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
+##################################################
+# VARIABLES
+##################################################
+# Name of the database where permissions, owners and paths are stored.
+database="permissions.db"
+
+
+##################################################
+# FUNCTIONS
+##################################################
+function install_packages(){
+    echo -e "\n\x1B[34mUpdating package list and system packages\x1B[0m"
+    pacman -Syyu --noconfirm
+    echo -e "\n\x1B[34mInstalling base packages (It may take a long time)\x1B[0m"
+    pacman --noconfirm -S yay nano gnupg xclip git base base-devel go pcsc-tools ccid gtk2 intel-ucode
+    sudo -H -u dragonis41 bash -c 'yay --answerclean All --answerdiff None --answeredit None --cleanafter --removemake --sudoloop -S bind linux61-headers autojump'
+}
+
+function install_extra_packages(){
+    echo -e "\n\x1B[34mUpdating package list and system packages\x1B[0m"
+    pacman -Syyu --noconfirm
+    echo -e "\n\x1B[34mInstalling extra packages (It may take a long time)\x1B[0m"
+    pacman -S yay --noconfirm
+    sudo -H -u dragonis41 bash -c 'yay --answerclean All --answerdiff None --answeredit None --cleanafter --removemake --sudoloop -S google-chrome jetbrains-toolbox burpsuite filezilla mattermost-desktop notepadqq postman-bin spotify thunderbird vlc realvnc-vnc-viewer realvnc-vnc-server hopenpgp-tools yubikey-personalization docker docker-compose docker-machine lazydocker'
+    systemctl enable --now docker.service
+    usermod -aG docker dragonis41
+}
+
+function install_ohmyzsh(){
+    echo -e "\n\x1B[34mInstalling oh-my-zsh with plugins\x1B[0m"
+    sudo pacman -S git --noconfirm
+    sudo rm -r /home/dragonis41/.oh-my-zsh/
+    sudo -H -u dragonis41 bash -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    sudo -H -u dragonis41 bash -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+    sudo -H -u dragonis41 bash -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+}
+
+function configure_yubikey(){
+    systemctl enable --now pcscd.socket
+
+    echo ""
+    read -p "Insert the Yubikey and press enter to continue" yn
+    case $yn in
+        * ) echo -e "\n\x1B[34mConfiguring Yubikey\x1B[0m";;
+    esac
+
+    sudo -H -u dragonis41 bash -c 'gpg --import /home/dragonis41/.gnupg/public.key'
+    echo "To continue, execute the following commands :"
+    echo -e "\x1B[33mgpg --edit-card\x1B[0m"
+    echo "Copy the secret key ID, then"
+    echo -e "\x1B[33mgpg --edit-key KEYID\x1B[0m"
+    echo -e "\x1B[33mtrust\x1B[0m"
+    echo -e "\x1B[33m5\x1B[0m"
+    echo -e "\x1B[33mquit\x1B[0m"
+    echo "Then, reboot."
+}
+
+function restore_config(){
+    echo -e "\n\x1B[34mRestoring configuration files\x1B[0m"
+    while read -r line
+    do
+        # Format : file_permission ; file_group ; file_path ; folder_permission ; folder_group ; folder_path ; backup_file
+        file_permission="$(echo $line | cut -d ";" -f1)"
+        file_group="$(echo $line | cut -d ";" -f2)"
+        file_path="$(echo $line | cut -d ";" -f3)"
+        folder_permission="$(echo $line | cut -d ";" -f4)"
+        folder_group="$(echo $line | cut -d ";" -f5)"
+        folder_path="$(echo $line | cut -d ";" -f6)"
+        backup_file="$(echo $line | cut -d ";" -f7)"
+
+        # create original folder with right permissions.
+        if [[ ! -e $folder_path ]]; then
+            mkdir "$folder_path" -m $folder_permission -p
+            if (($? != 0)); then
+                echo -e "\n\x1B[31mAn error occured will creating $folder_path folder\x1B[0m"
+                return 1
+            fi
+            chown -R $folder_group $folder_path
+            if (($? != 0)); then
+                echo -e "\n\x1B[31mAn error occured will chown $folder_path with $folder_group\x1B[0m"
+                return 1
+            fi
+        fi
+
+        # Copy each file by preserving its attributes.
+        cp -v --preserve=all "$backup_file" "$file_path"
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will copying $backup_file to $file_path\x1B[0m"
+            return 1
+        fi
+
+        # Set the right permission.
+        chmod $file_permission $file_path
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will chmod $file_path with $file_permission\x1B[0m"
+            return 1
+        fi
+
+        # Set the right group.
+        chown $file_group $file_path
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will chown $file_path with $file_group\x1B[0m"
+            return 1
+        fi
+    done < $database
+}
+
+
+##################################################
+# PROGRAM
+##################################################
+# Variables for choices prompt.
+var_install_packages=false
+var_install_extra=false
+var_install_ohmyzsh=false
+var_configure_yubikey=false
+
+function prompt_packages(){
+    read -p "Install base packages before restoring configuration files ? [yn] " yn
+    case $yn in
+        [Yy]* ) var_install_packages=true;;
+        [Nn]* ) var_install_packages=false;;
+        * ) echo "Please answer yes or no."; prompt_packages;;
+    esac
+}
+prompt_packages
+
+function prompt_extra_packages(){
+    read -p "Install extra packages before restoring configuration files ? [yn] " yn
+    case $yn in
+        [Yy]* ) var_install_extra=true;;
+        [Nn]* ) var_install_extra=false;;
+        * ) echo "Please answer yes or no."; prompt_extra_packages;;
+    esac
+}
+prompt_extra_packages
+
+function prompt_ohmyzsh(){
+    read -p "Install Oh My Zsh with plugins before restoring configuration files ? Warning : It will delete current installation [yn]  " yn
+    case $yn in
+        [Yy]* ) var_install_ohmyzsh=true;;
+        [Nn]* ) var_install_ohmyzsh=false;;
+        * ) echo "Please answer yes or no."; prompt_ohmyzsh;;
+    esac
+}
+prompt_ohmyzsh
+
+function prompt_yubikey(){
+    read -p "Configure the Yubikey ? [yn]" yn
+    case $yn in
+        [Yy]* ) var_configure_yubikey=true;;
+        [Nn]* ) var_configure_yubikey=false;;
+        * ) echo "Please answer yes or no."; prompt_yubikey;;
+    esac
+}
+prompt_yubikey
+
+
+if ($var_install_packages); then
+    install_packages
+fi
+if ($var_install_extra); then
+    install_extra_packages
+fi
+if ($var_install_ohmyzsh); then
+    install_ohmyzsh
+fi
+
+if ! restore_config; then
+    exit 1
+fi
+if ($var_configure_yubikey); then
+    configure_yubikey
+fi
+
+echo -e "\n\x1B[32mDone\x1B[0m"
+exit 0
---
+++ save_manjaro.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
+
+##################################################
+# VARIABLES
+##################################################
+# Name of the database where permissions, owners and paths are stored.
+database="permissions.db"
+# Full path of files to save.
+# Format: "source:dest".
+files=(
+    ####################### User config files #######################
+    "/home/dragonis41/.gnupg/gpg-agent.conf:Manjaro/dragonis41/.gnupg/"
+    "/home/dragonis41/.gnupg/public.key:Manjaro/dragonis41/.gnupg/"
+    "/home/dragonis41/.bashrc:Manjaro/dragonis41/"
+    "/home/dragonis41/.gitconfig:Manjaro/dragonis41/"
+    "/home/dragonis41/.nanorc:Manjaro/dragonis41/"
+    "/home/dragonis41/.p10k.zsh:Manjaro/dragonis41/"
+    "/home/dragonis41/.zshrc:Manjaro/dragonis41/"
+
+    ########################### Git Hooks ###########################
+    # Pre commit
+    "/home/dragonis41/.git-hooks/pre-commit:Manjaro/dragonis41/.git-hooks/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/01.check-for-private-key.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/02.check-for-env-files.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/03.check-end-of-file.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/04.format-go-files.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/05.validate-json-files.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    "/home/dragonis41/.git-hooks/pre-commit.d/06.validate-xml-files.hook:Manjaro/dragonis41/.git-hooks/pre-commit.d/"
+    # Commit msg
+    "/home/dragonis41/.git-hooks/commit-msg:Manjaro/dragonis41/.git-hooks/"
+    "/home/dragonis41/.git-hooks/commit-msg.d/01.check-message.hook:Manjaro/dragonis41/.git-hooks/commit-msg.d/"
+    # Post update
+    "/home/dragonis41/.git-hooks/post-update:Manjaro/dragonis41/.git-hooks/"
+    "/home/dragonis41/.git-hooks/post-update.d/01.update-server-info.hook:Manjaro/dragonis41/.git-hooks/post-update.d/"
+
+    ############################ System #############################
+    "/etc/pacman.conf:Manjaro/dragonis41/etc/"
+
+    ####################### Root config files #######################
+    "/root/.nanorc:Manjaro/root/"
+    "/root/.p10k.zsh:Manjaro/root/"
+    "/root/.zshrc:Manjaro/root/"
+)
+
+
+##################################################
+# FUNCTIONS
+##################################################
+function mkcp(){
+    file_source="$(echo "$@" | cut -d ":" -f 1)"  # Parse the first part of each line in the variable "files".
+    folder_dest="$(echo "$@" | cut -d ":" -f 2)"  # Parse the second part of each line in the variable "files".
+
+    # Format everything and add it to the database.
+    # Format : file_permission ; file_group ; file_path ; folder_permission ; folder_group ; folder_path ; backup_file
+    echo "$(stat -c "%a;%U:%G;%n" "$file_source");$(stat -c "%a;%U:%G;%n" $(dirname $file_source));$folder_dest$(basename $file_source)" >> $database
+
+    # Create destination folder.
+    if [[ ! -e $folder_dest ]]; then
+        mkdir "$folder_dest" -m 755 -p
+        chown dragonis41:dragonis41 "$folder_dest"
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will chmod $folder_dest\x1B[0m"
+            return 1
+        fi
+    fi
+
+    # Copy each file if it is different and newer preserving its attributes.
+    cp -uv --preserve=all "$file_source" "$folder_dest"
+    if (($? != 0)); then
+        echo -e "\n\x1B[31mAn error occured will cp $file_source to $folder_dest\x1B[0m"
+        return 1
+    fi
+
+    return 0
+}
+
+
+##################################################
+# DATABASE INIT
+##################################################
+echo -e "\n\x1B[34mResetting permission database\x1B[0m"
+echo -n "" > $database
+chmod 664 $database
+chown dragonis41:dragonis41 $database
+rm -r Manjaro/
+
+##################################################
+# COPY
+##################################################
+echo -e "\n\x1B[34mCopying files\x1B[0m"
+
+for f in "${files[@]}"
+do
+    if mkcp "$f"; then
+        continue
+    else
+        exit 1
+    fi
+done
+
+echo -e "\n\x1B[34mCleaning\x1B[0m"
+find ./Manjaro -type d -exec chmod 755 {} +
+find ./Manjaro -type f -exec chmod 664 {} +
+chown -R dragonis41:dragonis41 ./Manjaro/
+
+echo -e "\n\x1B[32mDone\x1B[0m"
+exit 0

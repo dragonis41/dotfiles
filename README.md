0dcb729ac54a40ff6ed2099f8b866ab490244f4f
Guillaume Pinon <dev@guillaume-pinon.eu>
2023-05-25 18:57:02 +0200

    feat: add a function to display a message at the end

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dotfiles
 My personnal dotfiles with custom backup and restore script.
+
+## Features
+### save_manjaro.sh
+It will :
+- Do a backup of all files listed in it.
+- Create a database with the path of all files and theirs permissions.
+
+### restore_manjaro.sh
+It will :
+- Install base packages
+- Install extra packages
+- Install oh-my-zsh
+- Install and configure fingerprint authentication
+- Install and configure Yubikey
+
+### permissions.db
+Do not touch !\
+Permissions and paths are stored in this file.
+
+### Manjaro/
+The folder where backup is stored
--- restore_manjaro.sh
+++ restore_manjaro.sh
@@ -47,20 +47,20 @@ function install_extra_packages(){
 }
 
 function install_ohmyzsh(){
-    echo -e "\n\x1B[34mInstalling oh-my-zsh with plugins\x1B[0m"
-    sudo pacman -S git --noconfirm
+    echo -e "\x1B[34mInstalling oh-my-zsh with plugins\x1B[0m"
+    pacman -S git --noconfirm
+
+    # Installing for root
+    rm -r /root/.oh-my-zsh/
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
     # Installing for dragonis41
-    sudo rm -r /home/dragonis41/.oh-my-zsh/
+    rm -r /home/dragonis41/.oh-my-zsh/
     sudo -H -u dragonis41 bash -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
     sudo -H -u dragonis41 bash -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
     sudo -H -u dragonis41 bash -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-
-    # Installing for root
-    sudo rm -r /root/.oh-my-zsh/
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/dragonis41/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
 function configure_yubikey(){
@@ -77,68 +77,107 @@ function configure_yubikey(){
     esac
 
     sudo -H -u dragonis41 bash -c 'gpg --import /home/dragonis41/.gnupg/public.key'
-    echo "To continue, execute the following commands :"
-    echo -e "\x1B[33mgpg --edit-card\x1B[0m"
-    echo "Copy the secret key ID, then"
-    echo -e "\x1B[33mgpg --edit-key KEYID\x1B[0m"
-    echo -e "\x1B[33mtrust\x1B[0m"
-    echo -e "\x1B[33m5\x1B[0m"
-    echo -e "\x1B[33mquit\x1B[0m"
-    echo "Then, reboot."
 }
 
 function restore_config(){
     echo -e "\n\x1B[34mRestoring configuration files\x1B[0m"
     while read -r line
     do
-        # Format : file_permission ; file_group ; file_path ; folder_permission ; folder_group ; folder_path ; backup_file
-        file_permission="$(echo $line | cut -d ";" -f1)"
-        file_group="$(echo $line | cut -d ";" -f2)"
-        file_path="$(echo $line | cut -d ";" -f3)"
-        folder_permission="$(echo $line | cut -d ";" -f4)"
-        folder_group="$(echo $line | cut -d ";" -f5)"
         folder_path="$(echo $line | cut -d ";" -f6)"
-        backup_file="$(echo $line | cut -d ";" -f7)"
 
-        # create original folder with right permissions.
-        if [[ ! -e $folder_path ]]; then
-            mkdir "$folder_path" -m $folder_permission -p
-            if (($? != 0)); then
-                echo -e "\n\x1B[31mAn error occured will creating $folder_path folder\x1B[0m"
-                return 1
+        # Restore files if
+        # - The folder is different than "/etc/pam.d"
+        # - The folder is "/etc/pam.d" and $var_install_fingerprint is true
+        if [ "$folder_path" == "/etc/pam.d" ]; then
+            if [ "$var_install_fingerprint" == "true" ]; then
+                copyfile $line
             fi
-            chown -R $folder_group $folder_path
-            if (($? != 0)); then
-                echo -e "\n\x1B[31mAn error occured will chown $folder_path with $folder_group\x1B[0m"
-                return 1
-            fi
-        fi
-
-        # Copy each file by preserving its attributes.
-        cp -v --preserve=all "$backup_file" "$file_path"
-        if (($? != 0)); then
-            echo -e "\n\x1B[31mAn error occured will copying $backup_file to $file_path\x1B[0m"
-            return 1
-        fi
-
-        # Set the right permission.
-        chmod $file_permission $file_path
-        if (($? != 0)); then
-            echo -e "\n\x1B[31mAn error occured will chmod $file_path with $file_permission\x1B[0m"
-            return 1
-        fi
-
-        # Set the right group.
-        chown $file_group $file_path
-        if (($? != 0)); then
-            echo -e "\n\x1B[31mAn error occured will chown $file_path with $file_group\x1B[0m"
-            return 1
+        else
+            copyfile $line
         fi
     done < $database
+}
 
-    echo "\nIf there is no fingerprint registered, execute the following command :"
-    echo -e "\x1B[33mfprintd-enroll \$(whoami)\x1B[0m"
-    echo -e "\x1B[33mfprintd-verify\x1B[0m"
+function copyfile(){
+    line=$1
+
+    # Format : file_permission ; file_group ; file_path ; folder_permission ; folder_group ; folder_path ; backup_file
+    file_permission="$(echo $line | cut -d ";" -f1)"
+    file_group="$(echo $line | cut -d ";" -f2)"
+    file_path="$(echo $line | cut -d ";" -f3)"
+    folder_permission="$(echo $line | cut -d ";" -f4)"
+    folder_group="$(echo $line | cut -d ";" -f5)"
+    folder_path="$(echo $line | cut -d ";" -f6)"
+    backup_file="$(echo $line | cut -d ";" -f7)"
+
+    # create original folder with right permissions.
+    if [[ ! -e $folder_path ]]; then
+        mkdir "$folder_path" -m $folder_permission -p
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will creating $folder_path folder\x1B[0m"
+            return 1
+        fi
+        chown -R $folder_group $folder_path
+        if (($? != 0)); then
+            echo -e "\n\x1B[31mAn error occured will chown $folder_path with $folder_group\x1B[0m"
+            return 1
+        fi
+    fi
+
+    # Copy each file by preserving its attributes.
+    cp -v --preserve=all "$backup_file" "$file_path"
+    if (($? != 0)); then
+        echo -e "\n\x1B[31mAn error occured will copying $backup_file to $file_path\x1B[0m"
+        return 1
+    fi
+
+    # Set the right permission.
+    chmod $file_permission $file_path
+    if (($? != 0)); then
+        echo -e "\n\x1B[31mAn error occured will chmod $file_path with $file_permission\x1B[0m"
+        return 1
+    fi
+
+    # Set the right group.
+    chown $file_group $file_path
+    if (($? != 0)); then
+        echo -e "\n\x1B[31mAn error occured will chown $file_path with $file_group\x1B[0m"
+        return 1
+    fi
+}
+
+function display_end_message(){
+    if ($var_install_fingerprint); then
+        echo -e "\n\x1B[34mEnd configuration : Fingerprint\x1B[0m"
+        function end_prompt_fingerprint(){
+            read -p "Do you want to register a fingerprint ? [yn]  " yn
+            case $yn in
+                [Yy]* )
+                    sudo -H -u dragonis41 bash -c 'fprind-enroll dragonis41'
+                    sudo -H -u dragonis41 bash -c 'fprintd-verify'
+                ;;
+                [Nn]* )
+                    echo "\nIf there is no fingerprint registered, execute the following command :"
+                    echo -e "\x1B[33mfprintd-enroll \$(whoami)\x1B[0m"
+                    echo -e "\x1B[33mfprintd-verify\x1B[0m"
+                ;;
+                * ) echo "Please answer yes or no."; end_prompt_fingerprint;;
+            esac
+        }
+        end_prompt_fingerprint
+    fi
+
+    if ($var_configure_yubikey); then
+        echo -e "\n\x1B[34mEnd configuration : Fingerprint\x1B[0m"
+        echo "To continue the configuration of the Yubikey, execute :"
+        echo -e "\x1B[33mgpg --edit-card\x1B[0m"
+        echo "Copy the secret key ID, then"
+        echo -e "\x1B[33mgpg --edit-key KEYID\x1B[0m"
+        echo -e "\x1B[33mtrust\x1B[0m"
+        echo -e "\x1B[33m5\x1B[0m"
+        echo -e "\x1B[33mquit\x1B[0m"
+        echo "Then, reboot."
+    fi
 }
 
 
@@ -149,10 +188,11 @@ function restore_config(){
 var_install_packages=false
 var_install_extra=false
 var_install_ohmyzsh=false
+var_install_fingerprint=false
 var_configure_yubikey=false
 
 function prompt_packages(){
-    read -p "Install base packages before restoring configuration files ? [yn] " yn
+    read -p "Install base packages ? [yn] " yn
     case $yn in
         [Yy]* ) var_install_packages=true;;
         [Nn]* ) var_install_packages=false;;
@@ -162,7 +202,7 @@ function prompt_packages(){
 prompt_packages
 
 function prompt_extra_packages(){
-    read -p "Install extra packages before restoring configuration files ? [yn] " yn
+    read -p "Install extra packages ? [yn] " yn
     case $yn in
         [Yy]* ) var_install_extra=true;;
         [Nn]* ) var_install_extra=false;;
@@ -172,7 +212,7 @@ function prompt_extra_packages(){
 prompt_extra_packages
 
 function prompt_ohmyzsh(){
-    read -p "Install Oh My Zsh with plugins before restoring configuration files ? Warning : It will delete current installation [yn]  " yn
+    read -p "Install Oh My Zsh with plugins ? Warning : It will delete current installation [yn]  " yn
     case $yn in
         [Yy]* ) var_install_ohmyzsh=true;;
         [Nn]* ) var_install_ohmyzsh=false;;
@@ -180,6 +220,16 @@ function prompt_ohmyzsh(){
     esac
 }
 prompt_ohmyzsh
+
+function prompt_fingerprint(){
+    read -p "Install fingerprint PAM config ? [yn] " yn
+    case $yn in
+        [Yy]* ) var_install_fingerprint=true;;
+        [Nn]* ) var_install_fingerprint=false;;
+        * ) echo "Please answer yes or no."; prompt_fingerprint;;
+    esac
+}
+prompt_fingerprint
 
 function prompt_yubikey(){
     read -p "Configure the Yubikey ? [yn]" yn
@@ -208,6 +258,8 @@ fi
 if ($var_configure_yubikey); then
     configure_yubikey
 fi
+
+display_end_message
 
 echo -e "\n\x1B[32mDone\x1B[0m"
 exit 0

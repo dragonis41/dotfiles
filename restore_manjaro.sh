8e2f39b88914e63d5af23bfe0faebeb59ebc9c8d
Guillaume Pinon <dev@guillaume-pinon.eu>
2025-06-03 15:35:49 +0200

    refactor: use SUDO_USER everywhere (issue #2) and move Tampermonkey scripts so they won't be deleted at each backup

--- restore_manjaro.sh
+++ restore_manjaro.sh
@@ -356,6 +356,17 @@ if [ "$EUID" -ne 0 ] || [ "$SUDO_USER" = "" ]; then
     display_error "Please run as your current user with sudo but not as root"
 fi
 
+function prompt_restore(){
+    echo -e "This script will restore files for user [$SUDO_USER], if the already backed up files do not match this username, you can break your system !"
+    read -rp "Continue ? [yn] " yn
+    case $yn in
+        [Yy]* ) var_install_packages=true;;
+        [Nn]* ) var_install_packages=false;;
+        * ) echo "Please answer yes or no."; prompt_restore;;
+    esac
+}
+prompt_restore
+
 function prompt_packages(){
     read -rp "Install base packages ? [yn] " yn
     case $yn in
